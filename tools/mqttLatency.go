589eb876b96a5d5c759882294ea3164c67cf50e1
Brandon <brandon.owen@hotmail.com>
2019-03-04 16:26:37 +1100

    Update MQTT Latency

--- tools/mqttLatency.go
+++ tools/mqttLatency.go
@@ -6,7 +6,7 @@ import (
     "os"
     "flag"
     MQTT "github.com/eclipse/paho.mqtt.golang"
-    "github.com/geoscienceaustralia/go-rtcm/pkgs/rtcm3"
+    "github.com/geoscienceaustralia/go-rtcm/rtcm3"
 )
 
 func main() {
@@ -22,13 +22,15 @@ func main() {
     opts.SetDefaultPublishHandler(func(client MQTT.Client, mqttMsg MQTT.Message) {
         rtcmMsg := rtcm3.DeserializeMessage(mqttMsg.Payload())
         switch int(rtcmMsg.Number()) {
-            case 1071, 1072, 1073, 1074, 1075, 1076, 1077,
-                 1081, 1082, 1083, 1084, 1085, 1086, 1087,
-                 1091, 1092, 1093, 1094, 1095, 1096, 1097,
-                 1111, 1112, 1113, 1114, 1115, 1116, 1117,
-                 1121, 1122, 1123, 1124, 1125, 1126, 1127,
-                 1001, 1002, 1003, 1004, 1009, 1010, 1011, 1012:
-                fmt.Println(mqttMsg.Topic(), rtcmMsg.Number(), time.Now().UTC().Sub(rtcmMsg.(rtcm3.Observable).Time()))
+        case 1071, 1072, 1073, 1074, 1075, 1076, 1077,
+             1081, 1082, 1083, 1084, 1085, 1086, 1087,
+             1091, 1092, 1093, 1094, 1095, 1096, 1097,
+             1111, 1112, 1113, 1114, 1115, 1116, 1117,
+             1121, 1122, 1123, 1124, 1125, 1126, 1127,
+             1001, 1002, 1003, 1004, 1009, 1010, 1011, 1012:
+            fmt.Println(mqttMsg.Topic(), rtcmMsg.Number(), time.Now().UTC().Sub(rtcmMsg.(rtcm3.Observable).Time()))
+        default:
+            fmt.Println(mqttMsg.Topic(), rtcmMsg.Number())
         }
     })
 
