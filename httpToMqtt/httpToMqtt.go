7a68e5a0085ee1c4734c695770c355ae1e7f0f42
Brandon <brandon.owen@hotmail.com>
2019-04-01 12:02:09 +1100

    Format sourcetable entries

--- httpToMqtt/httpToMqtt.go
+++ httpToMqtt/httpToMqtt.go
@@ -13,14 +13,51 @@ import (
 // TODO: Clients subscribe to Mount so each client doesn't need a MQTT connection
 type Mount struct {
 	Name string
-	SourceTableEntry string
+	// Used to decide whether or not an MQTT stream is still "active"
 	LastMessage time.Time
+	SourceInformation SourceInformation
 }
 
-var ( // config
+// These could more simply be attributes of Mount
+type SourceInformation struct {
+	Identifier string
+	Format string
+	FormatDetails string
+	Carrier string
+	NavSystem string
+	Network string
+	CountryCode string
+	Latitude float64
+	Longitude float64
+	NMEA bool
+	Solution bool
+	Generator string
+	Compression string
+	Authentication string
+	Fee bool
+	Bitrate int
+	Misc string
+}
+
+// NTRIP Sourcetable entry format
+func (mount *Mount) String() string {
+	return fmt.Sprintf("STR;%s;%s;%s;%s;%s;%s;%s;%s;%f;%f;%t;%t;%s;%s;%s;%t;%v;%s",
+		mount.Name, mount.SourceInformation.Identifier, mount.SourceInformation.Format,
+		mount.SourceInformation.FormatDetails, mount.SourceInformation.Carrier,
+		mount.SourceInformation.NavSystem, mount.SourceInformation.Network,
+		mount.SourceInformation.CountryCode, mount.SourceInformation.Latitude,
+		mount.SourceInformation.Longitude, mount.SourceInformation.NMEA,
+		mount.SourceInformation.Solution, mount.SourceInformation.Generator,
+		mount.SourceInformation.Compression, mount.SourceInformation.Authentication,
+		mount.SourceInformation.Fee, mount.SourceInformation.Bitrate, mount.SourceInformation.Misc)
+}
+
+var ( // TODO: Define mounts from config
 	mounts = map[string]*Mount{
-		"SYMY": &Mount{"SYMY", "Canberra (ACT);RTCM3.2;;;;;;;;;", time.Unix(0, 0)},
-		"TEST2": &Mount{"TEST2", "Sydney (NSW);RTCM3.2;;;;;;;;;", time.Unix(0, 0)},
+		"SYMY": &Mount{"SYMY", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
+		"TEST1": &Mount{"TEST1", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
+		"TEST2": &Mount{"TEST2", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
+		"TEST3": &Mount{"TEST3", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
 	}
 )
 
@@ -29,7 +66,7 @@ func main() {
 	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
-	defer mqttClient.Disconnect()
+	defer mqttClient.Disconnect(0)
 
 	for _, mount := range mounts { // Construct these from new mounts added to config
 		go func(mount *Mount) {
@@ -47,7 +84,7 @@ func main() {
 		if r.URL.Path == "/" { // Return sourcetable
 			for _, mount := range mounts {
 				if mount.LastMessage.After(time.Now().Add(-time.Second * 3)) {
-					fmt.Fprintf(w, "%s;%s\r\n", mount.Name, mount.SourceTableEntry)
+					fmt.Fprintf(w, "%s\r\n", mount)
 				}
 			}
 			w.(http.Flusher).Flush()
