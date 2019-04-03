ac2fda32c2cc342016fa9796b81fa5d8ab790361
Brandon <brandon.owen@hotmail.com>
2019-04-03 11:52:09 +1100

    Moving files around

--- httpToMqtt/httpToMqtt.go
+++ httpToMqtt/httpToMqtt.go
@@ -2,44 +2,44 @@
 package main
 
 import (
-	"time"
-	"log"
 	"fmt"
-	"net/http"
-	"github.com/geoscienceaustralia/go-rtcm/rtcm3"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/geoscienceaustralia/go-rtcm/rtcm3"
+	"log"
+	"net/http"
+	"time"
 )
 
 // TODO: Clients subscribe to Mount so each client doesn't need a MQTT connection
 type Mount struct {
 	Name string
 	// Used to decide whether or not an MQTT stream is still "active"
-	LastMessage time.Time
+	LastMessage       time.Time
 	SourceInformation SourceInformation
 }
 
 // These could more simply be attributes of Mount
 type SourceInformation struct {
-	Identifier string
-	Format string
-	FormatDetails string
-	Carrier string
-	NavSystem string
-	Network string
-	CountryCode string
-	Latitude string
-	Longitude string
-	NMEA string
-	Solution string
-	Generator string
-	Compression string
+	Identifier     string
+	Format         string
+	FormatDetails  string
+	Carrier        string
+	NavSystem      string
+	Network        string
+	CountryCode    string
+	Latitude       string
+	Longitude      string
+	NMEA           string
+	Solution       string
+	Generator      string
+	Compression    string
 	Authentication string
-	Fee string
-	Bitrate string
-	Misc string
+	Fee            string
+	Bitrate        string
+	Misc           string
 }
 
-// NTRIP Sourcetable entry format
+// String representation of Mount in NTRIP Sourcetable entry format
 func (mount *Mount) String() string {
 	return fmt.Sprintf("STR;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s",
 		mount.Name, mount.SourceInformation.Identifier, mount.SourceInformation.Format,
@@ -55,10 +55,10 @@ func (mount *Mount) String() string {
 var ( // TODO: Define mounts from config
 	broker = "tcp://35.189.48.59:1883"
 	mounts = map[string]*Mount{
-		"SYMY": &Mount{"SYMY", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
-		"TEST1": &Mount{"TEST1", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
-		"TEST2": &Mount{"TEST2", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
-		"TEST3": &Mount{"TEST3", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
+		"SYMY00AUS": &Mount{"SYMY00AUS", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
+		"TEST00AUS": &Mount{"TEST00AUS", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
+		"TEST04AUS": &Mount{"TEST04AUS", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
+		"TEST05AUS": &Mount{"TEST05AUS", time.Unix(0, 0), SourceInformation{Identifier: "Canberra (ACT)", Format: "RTCM3.2"}},
 	}
 )
 
@@ -71,7 +71,7 @@ func main() {
 
 	for _, mount := range mounts { // Create these subscriptions on initialization of / changes to config
 		go func(mount *Mount) { // This doesn't need to be a go routine, but mount does need to be copied so the anonymous function passed to Subscribe isn't a closure
-			token := mqttClient.Subscribe(mount.Name + "/#", 1, func(client mqtt.Client, msg mqtt.Message) {
+			token := mqttClient.Subscribe(mount.Name+"/#", 1, func(client mqtt.Client, msg mqtt.Message) {
 				mount.LastMessage = time.Now()
 			})
 			if token.Wait() && token.Error() != nil {
@@ -93,9 +93,9 @@ func main() {
 		}
 
 		switch r.Method {
-		case http.MethodGet: // Create MQTT connection on behalf of the client and subscribe to Topic of Mount.Name 
+		case http.MethodGet: // Create MQTT connection on behalf of the client and subscribe to Topic of Mount.Name
 			mount, exists := mounts[r.URL.Path[1:]]
-			if !exists || mount.LastMessage.Before(time.Now().Add(-time.Second * 3)) {
+			if !exists || mount.LastMessage.Before(time.Now().Add(-time.Second*3)) {
 				w.WriteHeader(http.StatusNotFound)
 				return
 			}
@@ -106,9 +106,10 @@ func main() {
 			if token := subClient.Connect(); token.Wait() && token.Error() != nil {
 				panic(token.Error())
 			}
+			defer subClient.Disconnect(0)
 
 			data := make(chan []byte)
-			token := subClient.Subscribe(mount.Name + "/#", 1, func(client mqtt.Client, msg mqtt.Message) {
+			token := subClient.Subscribe(mount.Name+"/#", 1, func(client mqtt.Client, msg mqtt.Message) {
 				data <- rtcm3.EncapsulateMessage(rtcm3.DeserializeMessage(msg.Payload())).Serialize()
 			})
 			if token.Wait() && token.Error() != nil {
@@ -120,17 +121,18 @@ func main() {
 				w.(http.Flusher).Flush()
 			}
 
-		case http.MethodPost: // Could currently have multiple sources POSTing to a topic
+		case http.MethodPost: // Could currently have multiple sources POSTing to the same topic
 			pubClient := mqtt.NewClient(mqtt.NewClientOptions().AddBroker(broker))
 			if token := pubClient.Connect(); token.Wait() && token.Error() != nil {
 				panic(token.Error())
 			}
+			defer pubClient.Disconnect(0)
 
 			w.Header().Set("Connection", "close")
 			w.(http.Flusher).Flush()
 			time.Sleep(1 * time.Second) // Why? Without this it looks like we're reading from the body before the POSTer has sent any data
 
-			scanner := rtcm3.NewScanner(r.Body)
+			scanner := rtcm3.NewScanner(r.Body) // Should probably just parse Frame, not Message
 			msg, err := scanner.Next()
 			for ; err == nil; msg, err = scanner.Next() {
 				pubClient.Publish(fmt.Sprintf("%s/%d", r.URL.Path[1:], msg.Number()), 1, false, msg.Serialize())
