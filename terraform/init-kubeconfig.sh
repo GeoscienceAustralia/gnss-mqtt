b1674fa7aa53f73f7c098f47e02fc713a0339779
Jonathan Mettes <jonathan@jmettes.com>
2019-05-01 14:35:54 +1000

    Fix EKS role init

--- terraform/init-kubeconfig.sh
+++ terraform/init-kubeconfig.sh
@@ -1,9 +1,14 @@
 terraform init -backend-config=backend.tfvars
 
 # setup KUBECONFIG
-terraform output kubeconfig > ~/.kube/auscors-eks-config
-export KUBECONFIG=~/.kube/auscors-eks-config
+aws eks update-kubeconfig --name auscors
 
 # assume role for kubectl
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
 role_arn=$(terraform output eks_kubectl_role_arn)
-aws sts assume-role --role-arn ${role_arn} --role-session-name auscors-eks
+aws_credentials_json=$(aws sts assume-role --role-arn ${role_arn} --role-session-name auscors-eks)
+export AWS_ACCESS_KEY_ID=$(echo "$aws_credentials_json" | jq --exit-status --raw-output .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo "$aws_credentials_json" | jq --exit-status --raw-output .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo "$aws_credentials_json" | jq --exit-status --raw-output .Credentials.SessionToken)
