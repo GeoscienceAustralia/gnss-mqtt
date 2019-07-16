1810a4b3613a0d3bce7c1b4da28e682724d71adb
Brandon <brandon.owen@hotmail.com>
2019-07-16 15:53:37 +1000

    mqtt-latency is a good example of where you would want to have an OnConnectHandler which subscribes to topics instead of disabling clean sessions

--- cmd/mqtt-latency/mqtt-latency.go
+++ cmd/mqtt-latency/mqtt-latency.go
@@ -15,15 +15,16 @@ func main() {
 
 	opts := mqtt.NewClientOptions().
 		AddBroker(*broker).
-		SetCleanSession(false).
-		SetClientID("mqtt-latency")
+		SetOnConnectHandler(func(client mqtt.Client) {
+			if token := client.Subscribe(*topic, 1, nil); token.Wait() && token.Error() != nil {
+				panic(token.Error())
+			}
+		})
+
 
 	opts.SetDefaultPublishHandler(func(client mqtt.Client, mqttmsg mqtt.Message) {
 		msg := rtcm3.DeserializeMessage(mqttmsg.Payload()) // DeserializeMessage should return error
 		if obs, ok := msg.(rtcm3.Observable); ok {
-			if msg.Number() == 1087 {
-				fmt.Printf("%+v\n\n", msg)
-			}
 			fmt.Println(time.Now().Format("2006-01-02T15:04:05.999999"), mqttmsg.Topic(), obs.Number(), len(mqttmsg.Payload()), time.Now().UTC().Sub(obs.Time()))
 		} else {
 			fmt.Println(time.Now().Format("2006-01-02T15:04:05.999999"), mqttmsg.Topic(), msg.Number(), len(mqttmsg.Payload()))
@@ -32,10 +33,6 @@ func main() {
 
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-
-	if token := client.Subscribe(*topic, 1, nil); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
