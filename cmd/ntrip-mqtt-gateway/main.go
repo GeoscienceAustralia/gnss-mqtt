eb5f5676c89d2ba05377b86b3ddf59531316e678
Brandon <brandon.owen@hotmail.com>
2019-09-19 22:39:29 +1000

    Use flags for config, will probably eventually need a config file

--- cmd/ntrip-mqtt-gateway/main.go
+++ cmd/ntrip-mqtt-gateway/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"flag"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	port := flag.String("ntrip-port", "2101", "")
+	broker := flag.String("mqtt-broker", "tcp://localhost:1883", "")
+	flag.Parse()
+
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetLevel(log.ErrorLevel)
 
 	// TODO: Define from config file
-	gateway, err := NewGateway("2101", "tcp://vernemq:1883")
+	gateway, err := NewGateway(*port, *broker)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	gateway.Hostname = "ntrip.geops.team"
+	gateway.Hostname = "streams.geops.team"
 	gateway.Operator = "Geoscience Australia"
 
 	log.Fatal(gateway.Serve())
